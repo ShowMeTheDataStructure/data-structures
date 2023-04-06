9f03a28d8af2cde8a9439ce24d6b6c691022731f
haejunejung <jhj9422@unist.ac.kr>
2023-04-06 20:42:42 +0900

    [#7] feat : linked list class template 작성

--- cplusplus-main/linkedList/linkedList.cpp
+++ cplusplus-main/linkedList/linkedList.cpp
@@ -1,7 +1,92 @@
 #include "linkedList.h"
 #include <iostream>
 
-void test()
+// constructor
+template <typename Type>
+LinkedList<Type>::LinkedList()
 {
-    std::cout << " linked list connection! " << std::endl;
+}
+
+// Destructor
+template <typename Type>
+LinkedList<Type>::~LinkedList()
+{
+}
+
+// Get the value located at index
+template <typename Type>
+Type LinkedList<Type>::Get(const int index)
+{
+}
+
+// Add val at head
+template <typename Type>
+void LinkedList<Type>::AddAtHead(const Type &val)
+{
+}
+
+// Add val at index
+template <typename Type>
+void LinkedList<Type>::AddAtIndex(const int index, const Type &val)
+{
+}
+
+// Delete an element located at index
+template <typename Type>
+void LinkedList<Type>::DeleteAtIndex(const int index)
+{
+}
+
+// Delete val in linked list
+template <typename Type>
+void LinkedList<Type>::MoveToHead(const Type &val)
+{
+}
+
+// Move the first element of val to head
+template <typename Type>
+void LinkedList<Type>::Rotate(const int steps)
+{
+}
+
+// Reduce value that repeats multiple times
+template <typename Type>
+void LinkedList<Type>::Reduce()
+{
+}
+
+// Reverse at every k number of nodes at a time
+template <typename Type>
+void LinkedList<Type>::K_Reverse(const int k)
+{
+}
+
+// Sort even and odd numbers separately then append
+template <typename Type>
+void LinkedList<Type>::EvenOddSeparateSort()
+{
+}
+
+// Return the number of element in the linked list
+template <typename Type>
+int LinkedList<Type>::Size()
+{
+}
+
+// Delete all elements from the linked list
+template <typename Type>
+void LinkedList<Type>::CleanUp()
+{
+}
+
+// Print the list
+template <typename Type>
+void LinkedList<Type>::Print()
+{
+}
+
+// check the linked list empty or not
+template <typename Type>
+bool LinkedList<Type>::empty()
+{
 }
--- cplusplus-main/linkedList/linkedList.h
+++ cplusplus-main/linkedList/linkedList.h
@@ -1,6 +1,69 @@
 #ifndef LINKEDLIST_H_
 #define LINKEDLIST_H_
 
-void test();
+#include <typeinfo>
+#include <iostream>
+
+template <typename Type>
+struct Node
+{
+    Type data;
+    Node *link;
+};
+
+template <typename Type>
+class LinkedList
+{
+private:
+    Node<Type> *head;
+    Node<Type> *tail;
+    int count;
+
+public:
+    // constructor
+    LinkedList();
+
+    // Destructor
+    ~LinkedList();
+
+    // Get the value located at index
+    Type Get(const int index);
+
+    // Add val at head
+    void AddAtHead(const Type &val);
+
+    // Add val at index
+    void AddAtIndex(const int index, const Type &val);
+
+    // Delete an element located at index
+    void DeleteAtIndex(const int index);
+
+    // Delete val in linked list
+    void MoveToHead(const Type &val);
+
+    // Move the first element of val to head
+    void Rotate(const int steps);
+
+    // Reduce value that repeats multiple times
+    void Reduce();
+
+    // Reverse at every k number of nodes at a time
+    void K_Reverse(const int k);
+
+    // Sort even and odd numbers separately then append
+    void EvenOddSeparateSort();
+
+    // Return the number of element in the linked list
+    int Size();
+
+    // Delete all elements from the linked list
+    void CleanUp();
+
+    // Print the list
+    void Print();
+
+    // check the linked list empty or not
+    bool empty();
+};
 
 #endif
