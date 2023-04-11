e6495adcd4717e1595200164ecfa5c4c412f8cdd
haejunejung <jhj9422@unist.ac.kr>
2023-04-12 00:26:13 +0900

    [#10] feat : queue template

---
+++ cplusplus-main/queue/queue.cpp
@@ -0,0 +1,22 @@
+#include "queue.h"
+
+template <typename Type>
+Queue<Type>::Queue() {}
+
+template <typename Type>
+Queue<Type>::~Queue() {}
+
+template <typename Type>
+void Queue<Type>::push(const Type &item) {}
+
+template <typename Type>
+void Queue<Type>::pop() {}
+
+template <typename Type>
+Type &Queue<Type>::front() const {}
+
+template <typename Type>
+bool Queue<Type>::empty() const {}
+
+template <typename Type>
+int Queue<Type>::size() const {}
