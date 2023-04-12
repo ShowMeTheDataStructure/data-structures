4b03c45c122759450d6b24c9d6d3d1881f77e88e
haejunejung <jhj9422@unist.ac.kr>
2023-04-12 19:37:50 +0900

    [#11] feat : LL namespace 추가

--- cplusplus-main/main.cpp
+++ cplusplus-main/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
+
+#include "linkedList/linkedList.cpp"
 #include "linkedList/linkedList.h"
 
 using namespace std;
 
-int main(void)
-{
+int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-
-    test();
 
     return 0;
 }
