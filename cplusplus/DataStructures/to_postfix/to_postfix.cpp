72742776a181c7d79c9652e4b61d48b5c214b238
haejunejung <jhj9422@unist.ac.kr>
2023-04-14 18:00:24 +0900

    [#24] feat : to_postfix, tree template 작성

---
+++ cplusplus/DataStructures/to_postfix/to_postfix.cpp
@@ -0,0 +1,11 @@
+#include "to_postfix.h"
+
+std::string ShowMeTheDataStructure::toPostfix::infixToPostfix(const std::string &infix)
+{
+    return "RETURN";
+}
+
+std::string ShowMeTheDataStructure::toPostfix::prefixToPostfix(const std::string &infix)
+{
+    return "RETURN";
+}
