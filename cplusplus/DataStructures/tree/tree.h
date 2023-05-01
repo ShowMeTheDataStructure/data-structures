aaf955fec8df2a18ee8d7169451541bf01c46ee5
haejunejung <jhj9422@unist.ac.kr>
2023-05-01 22:22:33 +0900

    [#30] chore : tree 주석처리 제거

--- cplusplus/DataStructures/tree/tree.h
+++ cplusplus/DataStructures/tree/tree.h
@@ -1,46 +1,48 @@
-// #ifndef TREE_H_
-// #define TREE_H_
+#ifndef TREE_H_
+#define TREE_H_
 
-// #include <iostream>
-// #include <sstream>
+#include <iostream>
+#include <sstream>
 
-// namespace ShowMeTheDataStructure {
-// /**
-//  * Recursive binary tree structure for building expression tree
-//  */
-// class Tree {
-//   private:
-//     std::string expr;
-//     Tree *left;
-//     Tree *right;
+namespace ShowMeTheDataStructure
+{
+    /**
+     * Recursive binary tree structure for building expression tree
+     */
+    class Tree
+    {
+    private:
+        std::string expr;
+        Tree *left;
+        Tree *right;
 
-//   public:
-//     // Constructor, type of h - character
-//     explicit Tree(char h, Tree *l = nullptr, Tree *r = nullptr);
+    public:
+        // Constructor, type of h - character
+        explicit Tree(char h, Tree *l = nullptr, Tree *r = nullptr);
 
-//     // Constructor, type of h - string
-//     explicit Tree(const std::string &h = std::string(), Tree *l = nullptr,
-//                   Tree *r = nullptr);
+        // Constructor, type of h - string
+        explicit Tree(const std::string &h = std::string(), Tree *l = nullptr,
+                      Tree *r = nullptr);
 
-//     // Destructor
-//     ~Tree();
+        // Destructor
+        ~Tree();
 
-//     // Size
-//     int size();
+        // Size
+        int size();
 
-//     // Print
-//     void print();
+        // Print
+        void print();
 
-//     // Eval
-//     double eval();
+        // Eval
+        double eval();
 
-//     const std::string value();
+        const std::string value();
 
-//     // build expression tree
-//     // Tree *build_expression_tree(const std::string &postfix);
-// };
+        // build expression tree
+        // Tree *build_expression_tree(const std::string &postfix);
+    };
 
-// Tree *build_expression_tree(const std::string &postfix);
-// } // namespace ShowMeTheDataStructure
+    Tree *build_expression_tree(const std::string &postfix);
+}; // namespace ShowMeTheDataStructure
 
-// #endif
+#endif
